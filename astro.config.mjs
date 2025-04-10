7e708f4e3509af5cacad9124f485128a866770db
Jhorky Escalante <jhorky2004@hotmail.com>
2025-04-10 20:36:06 -0300

    Correccion de mejoras en componentes y configuracion de proyecto, layout, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    resolve:{
+      alias: {
+        '@': path.resolve('./src'),
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@ui': path.resolve('./src/ui'),
+        '@icons': path.resolve('./src/icons'),
+        '@pages': path.resolve('./src/pages'),
+        '@styles': path.resolve('./src/styles'),
+      }
+    },
     plugins: [tailwindcss()]
   }
 });
