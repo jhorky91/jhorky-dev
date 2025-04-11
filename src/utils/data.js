7d8530bac0a42a4a660f677dcdc6bce31d836566
Jhorky Escalante <jhorky2004@hotmail.com>
2025-04-11 04:23:36 -0300

    retoques en estilos y nueva seccion proyectos

---
+++ src/utils/data.js
@@ -0,0 +1,51 @@
+export const EXPERIENCE = [
+  {
+    role: "Analista Funcional",
+    company: "Cladd I.T.A. S.A.",
+    date: "may. 2024 - nov. 2024",
+    description: [
+      "Soporte funcional a usuarios mediante el ERP de la empresa.",
+      "Gestión de tickets y resolución en plazos establecidos.",
+      "Colaboración con Desarrollo para detectar bugs y analizar logs.",
+      "Migración del sistema de producción.",
+      "Documentación de requerimientos y soluciones."
+    ],
+  },
+  {
+    role: "Analista Funcional",
+    company: "Accenture Argentina S.A.",
+    date: "jul. 2022 - abr. 2024",
+    description: [
+      "Soporte funcional (testing, mejoras, análisis de datos).",
+      "Ejecución y seguimiento de pruebas de usuario.",
+      "Detección y análisis de bugs, performance y regresiones.",
+      "Consultas SQL (Oracle, MySQL, SQL Server).",
+      "Revisión de ETLs (DataStage), jobs (Control-M) y BI.",
+      "Gestión de tickets y documentación de soluciones en SAP Solman."
+    ],
+  },
+  {
+    role: "Analista Funcional en Gestión de Apps",
+    company: "Sofrecom Argentina",
+    date: "sep. 2021 - jun. 2022",
+    description: [
+      "Monitoreo de procesos ETL (PowerCenter).",
+      "Manejo de reportería (MicroStrategy).",
+      "Gestión de datos en Teradata, Oracle y Hive (NiFi).",
+      "Gestión de incidencias (HP Service Manager).",
+      "Soporte en guardias técnicas."
+    ],
+  },
+  {
+    role: "Analista Funcional",
+    company: "Power Solution",
+    date: "sep. 2018 - ene. 2021",
+    description: [
+      "Mantenimiento de apps web y optimización de funcionalidades.",
+      "Documentación funcional de requerimientos y casos de uso.",
+      "Resolución de incidentes con HP Service Manager.",
+      "Coordinación de pruebas funcionales en QA y producción.",
+      "Soporte a usuarios finales y colaboración con desarrollo."
+    ],
+  },
+];
